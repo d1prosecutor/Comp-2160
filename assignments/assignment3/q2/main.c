5f73f29054ab0252d8d0e787c4ea8e016bbb27a5
Paradox <paradoxlegendz@gmail.com>
2022-11-19 21:23:03 -0600

    .

--- assignments/assignment3/q2/main.c
+++ assignments/assignment3/q2/main.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
-#include "main.h"
+
+#include "set.h"
+
+//--------------------------------------------------------------
+// PROTOTYPES
+//-------------------------------------------------------------
+void testNewSet(Set *, Boolean);
+
+void testDeleteSet(Set *, Boolean);
+
+void testInsertItem(Set *const, const int, Boolean);
+
+void testRemoveItem(Set *const testSet, const int givenItem, Boolean expected);
+
+void testAreEqual(Set const *, Set const *, Boolean);
+
+void testAreDisjoint(Set const *, Set const *, Boolean);
+
+void testUnionOf(Set const *, Set const *, Set const *);
+
+void testSymmetricDifferenceOf(Set const *, Set const *, Set const *);
+
+void testValidateMemUse(Boolean);
 
 // Variable declarations
 int testsFailed = 0;
