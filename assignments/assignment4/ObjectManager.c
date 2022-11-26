29d3169f3f2c420fbb915d012ac71471c91186c7
Paradox <paradoxlegendz@gmail.com>
2022-11-26 11:39:40 -0600

    add and drop reference, dump pool

--- assignments/assignment4/ObjectManager.c
+++ assignments/assignment4/ObjectManager.c
@@ -1,13 +1,14 @@
 #include "ObjectManager.h"
 #include <stdlib.h>
+#include <assert.h>
 
 typedef struct NODE Node;
 struct NODE
 {
     Ref numBytes;
     void *startAddress;
-    int reference;
-    int count;
+    Ref referenceID;
+    Ref count;
 
     Node *next;
 };
@@ -15,7 +16,7 @@ struct NODE
 struct MEMORYPOOL
 {
     Node *top;
-    int numNodes;
+    Ref numNodes;
     void *freePtr;
     void *endOfBuffer;
 };
@@ -58,16 +59,17 @@ void initPool()
 // On failure it returns NULL_REF (0)
 Ref insertObject(const int size)
 {
-    Ref refResult = 0;
+    Ref refResult = NULL_REF;
 
     assert(NULL != heapMemory);
 
     if (NULL != heapMemory)
     {
-        assert(heapMemory->freePtr + size < heapMemory->endOfBuffer);
-        // If there is still memory avaiable to allocate
         if (heapMemory->freePtr + size < heapMemory->endOfBuffer)
         {
+            // If there is still memory avaiable to allocate
+            assert(heapMemory->freePtr + size <= heapMemory->endOfBuffer);
+
             Node *curr = heapMemory->top;
             Node *prev = NULL;
 
@@ -78,8 +80,8 @@ Ref insertObject(const int size)
                 curr = curr->next;
             }
 
-            // Allocate the specified memory
-            Node *newNode = malloc(size);
+            // Making a new Node for the linked list
+            Node *newNode = malloc(sizeof(Node));
 
             assert(NULL != newNode);
             if (NULL != newNode)
@@ -89,7 +91,7 @@ Ref insertObject(const int size)
                 newNode->numBytes = size;
                 newNode->startAddress = heapMemory->freePtr;
                 newNode->count = 1; // number of objects pointing to this address
-                newNode->reference = heapMemory->numNodes++;
+                newNode->referenceID = heapMemory->numNodes++;
                 newNode->next = NULL; // The next node will always be null since insertion is occuring at the end
 
                 if (NULL == prev)
@@ -106,8 +108,17 @@ Ref insertObject(const int size)
                 // Shift the position of the free memory pointer to point to the next available space
                 heapMemory->freePtr += size;
 
-                refResult = newNode->reference;
+                refResult = newNode->referenceID;
+
+                // assert that the memory now contains at least one element
+                assert(NULL != heapMemory->top);
             }
+        }
+        else
+        {
+            assert(heapMemory->freePtr + size > heapMemory->endOfBuffer);
+
+            /// Garbage collection here
         }
     }
 
@@ -117,17 +128,94 @@ Ref insertObject(const int size)
 // returns a pointer to the object being requested given by the reference id
 void *retrieveObject(const Ref ref)
 {
+    int objectNotFound = 1; // boolean variable for searching for the object
+    void *result = NULL;
+
+    Node *curr = heapMemory->top;
+
+    assert(NULL != curr);
+    while ((NULL != curr) && objectNotFound)
+    {
+        if (ref == curr->referenceID)
+        {
+            result = curr->startAddress;
+            objectNotFound = 0;
+
+            assert(ref == curr->referenceID);
+        }
+        curr = curr->next;
+    }
+
+    return result;
 }
 
 // update our index to indicate that we have another reference to the given object
-void addReference(const Ref ref);
+void addReference(const Ref ref)
+{
+    int objectNotFound = 1; // boolean variable for searching for the object
+
+    Node *curr = heapMemory->top;
+
+    assert(NULL != curr);
+    while ((NULL != curr) && objectNotFound)
+    {
+        if (ref == curr->referenceID)
+        {
+            assert(ref == curr->referenceID);
+
+            curr->count++;
+            objectNotFound = 0;
+        }
+        curr = curr->next;
+    }
+}
 
 // update our index to indicate that a reference is gone
-void dropReference(const Ref ref);
+void dropReference(const Ref ref)
+{
+    int objectNotFound = 1; // boolean variable for searching for the object
+
+    Node *curr = heapMemory->top;
+    Node *prev = NULL;
+
+    assert(NULL != curr);
+    while ((NULL != curr) && objectNotFound)
+    {
+        if (ref == curr->referenceID)
+        {
+            assert(ref == curr->referenceID);
+
+            curr->count--;
+            objectNotFound = 0;
+        }
+        prev = curr;
+        curr = curr->next;
+    }
+
+    // If there are no more objects pointing to that address, unlink it from the list
+    if (NULL != curr && curr->count == 0)
+    {
+        assert(curr->count == 0);
+
+        prev->next = curr->next;
+    }
+}
 
 // clean up the object manager (before exiting)
 void destroyPool();
 
 // This function traverses the index and prints the info in each entry corresponding to a block of allocated memory.
 // You should print the block's reference id, it's starting address, and it's size (in bytes).
-void dumpPool();
+void dumpPool()
+{
+    Node *curr = heapMemory->top;
+
+    while (NULL != curr)
+    {
+        assert(NULL != curr);
+
+        printf("Block ID: %lu\n", curr->referenceID);
+        printf("Starting Address: %p\n", curr->startAddress);
+        printf("Block Size: %lu bytes\n\n", curr->numBytes);
+    }
+}
--- assignments/assignment4/main0.c
+++ assignments/assignment4/main0.c
@@ -1,31 +1,32 @@
 #include "ObjectManager.h"
 #include <stdio.h>
+#include <stdio.h>
 
 int main(int argc, char *argv[])
 {
   char *ptr;
   int i;
-  int id1,id2,id3;
+  int id1, id2, id3;
   initPool();
-  
-  id1= insertObject(100);
-  ptr = (char*)retrieveObject(id1);
+
+  id1 = insertObject(100);
+  ptr = (char *)retrieveObject(id1);
   for (i = 0; i < 100; i++)
-    ptr[i] = (char)(i%26 + 'A');
-  
+    ptr[i] = (char)(i % 26 + 'A');
+
   id2 = insertObject(4000);
   id3 = insertObject(20200);
   dropReference(id3);
   id3 = insertObject(10);
-  
-  ptr = (char*)retrieveObject(id1);
+
+  ptr = (char *)retrieveObject(id1);
   for (i = 0; i < 100; i++)
-    fprintf(stderr,"%c",ptr[i]);
-  fprintf(stderr,"\n");
-  
+    fprintf(stderr, "%c", ptr[i]);
+  fprintf(stderr, "\n");
+
   dumpPool();
-  destroyPool();
-  fprintf(stderr,"---\n");
+  // destroyPool();
+  // fprintf(stderr,"---\n");
+
   return 0;
 }
-
